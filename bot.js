d73c4b4bb04e7ae8b2ce785ba45f800d6b2e5dd3
borisimple <borisimple@gmail.com>
2020-08-06 14:57:28 +0200

    simplyfied bot commands

--- bot.js
+++ bot.js
@@ -6,20 +6,17 @@ const bot = new Telegraf(process.env["TELEGRAM_TOKEN"]);
 const CRYPTOPANIC_API_KEY = process.env["CRYPTOPANIC_KEY"];
 const cpanicURL = `https://cryptopanic.com/api/v1/portfolio/?auth_token=${CRYPTOPANIC_API_KEY}`;
 
-bot.command("start", ({ reply }) => {
-  return (
-    reply(
-      `How can I help you? 🤔`,
-      Markup.keyboard([["💰 Portfolio"]])
-        .oneTime()
-        .resize()
-        .extra()
-    ),
-    Markup.removeKeyboard()
-  );
-});
-
-bot.hears("💰 Portfolio", (ctx) => {
+bot.command("portfolio", (ctx) => {
+  // return (
+  //   reply(
+  //     `How can I help you? 🤔`,
+  //     Markup.keyboard([["💰 Portfolio"]])
+  //       .oneTime()
+  //       .resize()
+  //       .extra()
+  //   ),
+  //   Markup.removeKeyboard()
+  // );
   let coins = fetch(cpanicURL);
   coins.then((res) => {
     if (res.status !== 200) {
@@ -41,5 +38,28 @@ bot.hears("💰 Portfolio", (ctx) => {
       });
   });
 });
+
+// bot.hears("💰 Portfolio", (ctx) => {
+//   let coins = fetch(cpanicURL);
+//   coins.then((res) => {
+//     if (res.status !== 200) {
+//       console.log(`HTTP response error: ${res.status}`);
+//       return "Error 😢";
+//     }
+//     res
+//       .json()
+//       .then((data) => {
+//         let amounts = "";
+//         for (let [key, value] of Object.entries(data.portfolio.totals)) {
+//           amounts += `${key}: ${value}\n`;
+//         }
+//         ctx.replyWithHTML(`<b style="color: red">PORTFOLIO</b>\n\n${amounts}`);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         ctx.reply(`Error occured: ${err}`);
+//       });
+//   });
+// });
 
 bot.launch();
