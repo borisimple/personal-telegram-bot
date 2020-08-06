163450c57fb4d80007614f894c71aa05ee1ac747
borisimple <borisimple@gmail.com>
2020-08-06 15:33:02 +0200

    fixed command name

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const fetch = require("node-fetch");
 const { Composer } = require("micro-bot");
 
 const bot = new Composer();
-const CRYPTOPANIC_API_KEY = process.env["CRYPTOPANIC_KEY"];
+const CRYPTOPANIC_API_KEY = process.env.CRYPTOPANIC_KEY;
 const cpanicURL = `https://cryptopanic.com/api/v1/portfolio/?auth_token=${CRYPTOPANIC_API_KEY}`;
 
-bot.command("portrfolio", (ctx) => {
+bot.command("portfolio", (ctx) => {
   let coins = fetch(cpanicURL);
   coins.then((res) => {
     if (res.status !== 200) {
