5766466e8b6add8542f33a3bd25a856a59f16bf5
borisimple <borisimple@gmail.com>
2020-08-23 14:53:35 +0200

    removed import from bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,5 @@
-const fetch = require("node-fetch");
 const { Composer, Markup } = require("micro-bot");
-
 const bot = new Composer();
-const CRYPTOPANIC_API_KEY = process.env.CRYPTOPANIC_KEY;
-const cpanicURL = `https://cryptopanic.com/api/v1/portfolio/?auth_token=${CRYPTOPANIC_API_KEY}`;
-
 const portfolio = require("./portfolio");
 
 bot.command("start", ({ reply }) => {
