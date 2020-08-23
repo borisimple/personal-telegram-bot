c4aabcb96262664ab36a9a4abbe00db216e9918f
borisimple <borisimple@gmail.com>
2020-08-23 14:55:31 +0200

    reverted changes

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
+const fetch = require("node-fetch");
 const { Composer, Markup } = require("micro-bot");
+
 const bot = new Composer();
-const portfolio = require("./portfolio");
+const CRYPTOPANIC_API_KEY = process.env.CRYPTOPANIC_KEY;
+const cpanicURL = `https://cryptopanic.com/api/v1/portfolio/?auth_token=${CRYPTOPANIC_API_KEY}`;
 
 bot.command("start", ({ reply }) => {
   return (
@@ -16,7 +19,27 @@ bot.command("start", ({ reply }) => {
 });
 
 bot.hears("💰 Portfolio", (ctx) => {
-  portfolio(ctx);
+  ctx.reply(`Date: ${new Date().toLocaleDateString()}`);
+  let coins = fetch(cpanicURL);
+  coins.then((res) => {
+    if (res.status !== 200) {
+      console.log(`HTTP response error: ${res.status}`);
+      return "Error 😢";
+    }
+    res
+      .json()
+      .then((data) => {
+        let amounts = "";
+        for (let [key, value] of Object.entries(data.portfolio.totals)) {
+          amounts += `${key}: ${value}\n`;
+        }
+        ctx.replyWithHTML(`<b style="color: red">PORTFOLIO</b>\n\n${amounts}`);
+      })
+      .catch((err) => {
+        console.log(err);
+        ctx.reply(`Error occurred: ${err}`);
+      });
+  });
 });
 
 module.exports = bot;
